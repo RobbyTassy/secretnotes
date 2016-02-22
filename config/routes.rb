d902f01e81fc7b7a74c87216de2a016e2b821eb4
Robert Tassy <Rtass001@fiu.edu>
2016-02-22 10:55:03 -0500

    spike out signup form

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,6 @@
 Rails.application.routes.draw do
-  get 'users/new'
+  # this means edit user path with be /user/edit (singular)
+  resource :users
 
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
