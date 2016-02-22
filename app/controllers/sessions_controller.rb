1f63be437805eca41a3ce7e35b170bef9b0a7489
Robert Tassy <Rtass001@fiu.edu>
2016-02-22 11:38:41 -0500

    logging in

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -1,4 +1,16 @@
 class SessionsController < ApplicationController
   def new
   end
+
+  def create
+    @user = User.find_by(username: params[:username]).try(:authenticate, params[:password])
+    # definition of ".try": asks object if it can authenticate if so return or not return nil
+
+    return render action: 'new' unless @user
+
+    #logged in, horray
+    session[:user_id] = @user.id
+    redirector_to notes_path
+  end
+
 end
